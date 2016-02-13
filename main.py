eabb0fb3f5d9d681357c806a8630152257b76b25
Kasra Rahjerdi <contact@kasrarahjerdi.com>
2016-02-13 17:16:08 -0500

    Parse/save relevant info from matches

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from riotwatcher import RiotWatcher, NORTH_AMERICA, KOREA, EUROPE_WEST
 from datetime import datetime, timedelta
 from little_pger import LittlePGer
 from pprint import pprint
+import random
 import json
 import redis
 import time
@@ -22,13 +23,20 @@ def get_static(cache_ignore=False):
         champions = redis.get('static_champions')
         items = redis.get('static_items')
         if champions and items:
-            return json.loads(champions), json.loads(items)
+            champions = json.loads(champions)
+            items = json.loads(items)
+            items = {int(x):y for x,y in items.iteritems()}
+            return champions, items
 
-    champions = riot.static_get_champion_list()['data']
-    items = riot.static_get_item_list()['data']
+    champions = riot.static_get_champion_list(champ_data='tags,info')['data']
+    items = riot.static_get_item_list(item_list_data='depth,from')['data']
+    items = {int(x):y for x,y in items.iteritems()}
     redis.set('static_champions', json.dumps(champions))
     redis.set('static_items', json.dumps(items))
     return champions, items
+
+champion_data, item_data = get_static(cache_ignore=True)
+champion_data_by_id = {y['id']:y for x,y in champion_data.iteritems()}
 
 
 def get_players(cache_ignore=False):
@@ -100,14 +108,8 @@ def get_matches_for_champion(players, champ, begin_time=datetime.utcnow() - time
     return matches
 
 
-# def create_tables():
-    # with LittlePGer(conn=DATABASE_URL) as pg:
-    #     pg.cursor.execute("CREATE TABLE IF NOT EXISTS matches;")
-
-
 def save_matches_info(matches, champ):
-    all_items = []
-    for m in matches[:5]:
+    for m in matches:
         match = redis.hget('match_infos', '{}_{}'.format(m['region'], m['matchId']))
         if match:
             match = json.loads(match)
@@ -115,13 +117,58 @@ def save_matches_info(matches, champ):
             match = riot.get_match(m['matchId'], region=m['region'].lower(), include_timeline=True)
             redis.hset('match_infos', '{}_{}'.format(m['region'], m['matchId']), json.dumps(match))
         participantId = None
+        participant = None
         for p in match['participants']:
             if p['championId'] == champ['id']:
+                participant = p
                 participantId = p['participantId']
                 break
-        if not participantId:
-            print "Uhh no {} in this {}/{}".format(champ['name'], m['region'], m['matchId'])
-            return
+        team = None
+        for t in match['teams']:
+            if t['teamId'] == participant['teamId']:
+                team = t
+                break
+
+        won = team['winner']
+        stats = participant['stats']
+        kills, deaths, assists = stats['kills'], stats['deaths'], stats['assists']
+        first_blood = stats['firstBloodKill']
+        first_blood_assist = stats['firstBloodAssist']
+
+        tanks_friendly_team = 0
+        tanks_enemy_team = 0
+        lane_partner = None
+        for p in match['participants']:
+            friendly = p['teamId'] == participant['teamId']
+            tags = champion_data_by_id[p['championId']]
+            if 'Tank' in tags:
+                if friendly:
+                    tanks_friendly_team += 1
+                else:
+                    tanks_enemy_team += 1
+
+            if friendly:
+                continue
+
+            lane, role = None, None
+            for t in p['timeline']:
+                if 'lane' not in t or 'role' not in t:
+                    continue
+                lane = t['lane']
+                role = t['role']
+                break
+            if not (lane and role):
+                continue
+            raise ValueError('{}, {}'.format(lane, role))
+            if lane == m['lane'] and role == m['role']:
+                lane_partner = p
+                break
+
+        if lane_partner:
+            lane_partner_champ = champion_data_by_id[lane_partner['championId']]
+            lane_partner_ad = lane_partner_champ['info']['attack'] > lane_partner['info']['magic']
+        else:
+            lane_partner_ad = False
 
         items = []
         for e in match['timeline']['frames']:
@@ -132,20 +179,73 @@ def save_matches_info(matches, champ):
                     # I don't care about biscuits or health potions or wards or trinkets
                     if ev['itemId'] in (2003, 2010, 2043, 3340, 3341, 3361, 3362, 3363, 3364):
                         continue
+                    item_info = item_data[ev['itemId']]
+                    # Don't care about base items, only upgrades
+                    if 'depth' not in item_info:
+                        continue
+                    # Don't care about level 3 boot upgrades
+                    if 'group' in item_info and 'boots' in item_info['group'].lower():
+                        continue
                     items.append(ev['itemId'])
-        all_items.append(items)
-    return all_items
+
+        trimmed_items = []
+        for i, item_id in enumerate(items):
+            item = item_data[item_id]
+
+            prev_items = trimmed_items[:]
+            if prev_items and 'from' in item:
+                # if the last few items all build into this item, but they're
+                # different parts of the tree (i.e. they don't upgrade into each other)
+                # we shouldn't have the entire build path in the item list
+                from_items = item['from']
+                last_item = prev_items.pop()
+                while str(last_item) in from_items:
+                    if 'from' in item_data[last_item]:
+                        from_items += item_data[last_item]['from']
+                    trimmed_items.remove(last_item)
+                    if not prev_items:
+                        break
+                    last_item = prev_items.pop()
+            prev_items = trimmed_items[:]
+            if prev_items and 'from' in item:
+                # if the N-1th or N-2nd item is something that upgrades into this, skip it
+                last_item = prev_items.pop()
+                last_last_item = prev_items.pop() if prev_items else None
+
+                if str(last_item) in item['from']:
+                    trimmed_items.remove(last_item)
+                if str(last_last_item) in item['from']:
+                    trimmed_items.remove(last_last_item)
+
+            trimmed_items.append(item_id)
+
+        items = trimmed_items
+
+        details = {
+            'championId': champ['id'],
+            'won': won,
+            'duration': match['matchDuration'],
+            'kills': kills,
+            'deaths': deaths,
+            'assists': assists,
+            'first_blood_kill': first_blood,
+            'first_blood_assist': first_blood_assist,
+            'lane_enemy_ad': lane_partner_ad,
+            'purchases': items
+        }
+
+        redis.hset('match_details', "{}_{}_{}".format(m['region'], m['matchId'], champ['id']), json.dumps(details))
+        print "Parsed and saved match {}\n".format(m['matchId'])
+        pprint(details)
+        print "-" * 30
 
 
 if __name__ == "__main__":
-    champions, items = get_static()
     players = get_players()
     print "{} players found".format(sum([len(y) for x,y in players.iteritems()]))
 
-    matches = get_matches_for_champion(players, champions['Viktor'])
+    matches = get_matches_for_champion(players, champion_data['Viktor'])
     print "Got {} Viktor matches".format(len(matches))
-    # create_tables()
-    items_bought = save_matches_info(matches, champions['Viktor'])
-    for game in items_bought:
-        print " > ".join([items[str(x)]['name'] for x in game])
-        print ""
+    save_matches_info(matches, champion_data['Viktor'])
+    print "Saved {} match infos".format(len(matches))
+
